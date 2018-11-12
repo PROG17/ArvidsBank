a7d7641abd025938f2255259eb3a471aab6ec527
Arvid Zetterberg <zetterberg.arvid@gmail.com>
2018-11-12 11:51:11 +0100

    #2 - Feautre transaction/withdrawal

---
+++ ArvidsBowling/Models/Transaction/TransactionVM.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ArvidsBank.Models.Transaction
+{
+    public class TransactionVM
+    {
+        [Display(Name = "Belopp")]
+        public decimal Amount { get; set; }
+
+        [Display(Name = "Kontonummer")]
+        [Required(ErrorMessage = "Kontonummer måste anges")]
+        public string AccountNum { get; set; }
+    }
+}
